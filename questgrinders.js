34990fe8f798dafabf4f25068679314d267c3683
goodiesohhi <goodiesohhi@hotmail.com>
2016-12-03 10:22:25 -0500

    CLICK

--- questgrinders.js
+++ questgrinders.js
@@ -1,5 +1,301 @@
 if (Meteor.isClient) {
+ Template.game.game = function(){
+  var emitter;
+      var player;
+       var facing = 'left';
+       var jumpTimer = 0;
+       var cursors;
+       var jumpButton;
+       var bg;
+       var gold;
+  var fields =  {
 
+      preload: function() {
+
+          game.load.image('dude', '/hero.png');
+          game.load.image('background', '/background.jpg');
+          game.load.image('shack', '/assets/shack.png');
+  game.load.image('gold', 'assets/gold.png');
+
+      },
+
+      create: function() {
+
+        text = game.add.text(500, 500, "GOLD", {
+               font: "18px Arial",
+               fill: "#ff0044",
+               align: "right"
+           });
+
+     game.physics.startSystem(Phaser.Physics.ARCADE);
+
+
+  game.stage.backgroundColor = 0x337799;
+
+
+  emitter = game.add.emitter(0, 0, 100);
+
+
+  emitter.makeParticles('gold');
+
+  emitter.gravity = 200;
+
+
+  game.input.onDown.add(particleBurst, this);
+         game.world.setBounds(0, 0, 1920, 600);
+           game.physics.startSystem(Phaser.Physics.ARCADE);
+
+          game.time.desiredFps = 30;
+
+          bg = game.add.tileSprite(0, 0, 800, 600, 'background');
+
+          game.physics.arcade.gravity.y = 250;
+
+          player = game.add.sprite(20, 32, 'dude');
+   shack = game.add.sprite(20, 32, 'dude');
+
+          game.physics.enable(player, Phaser.Physics.ARCADE);
+
+          player.body.bounce.y = 0.2;
+          player.body.collideWorldBounds = true;
+          player.body.setSize(40, 125);
+
+
+
+          cursors = game.input.keyboard.createCursorKeys();
+          jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        game.camera.follow(player);
+      },
+
+
+
+
+      update:  function() {
+
+          // game.physics.arcade.collide(player, layer);
+         gold=Meteor.user().money;
+         text.setText("GOLD:" + gold+"G");
+
+          player.body.velocity.x = 0;
+
+          if (cursors.left.isDown)
+          {
+              player.body.velocity.x = -150;
+
+              if (facing != 'left')
+              {
+                  player.animations.play('left');
+                  facing = 'left';
+              }
+          }
+          else if (cursors.right.isDown)
+          {
+              player.body.velocity.x = 150;
+
+              if (facing != 'right')
+              {
+                  player.animations.play('right');
+                  facing = 'right';
+              }
+          }
+  	else if (cursors.up.isDown )
+          {
+
+
+                  window.location.href = "https://questgrinders.herokuapp.com/store";
+
+          }
+          else
+          {
+              if (facing != 'idle')
+              {
+                  player.animations.stop();
+
+                  if (facing == 'left')
+                  {
+                      player.frame = 0;
+                  }
+                  else
+                  {
+                      player.frame = 5;
+                  }
+
+                  facing = 'idle';
+              }
+          }
+
+          if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer)
+          {
+              player.body.velocity.y = -250;
+              jumpTimer = game.time.now + 750;
+          }
+
+      },
+
+      render:  function() {
+
+          game.debug.text(game.time.suggestedFps, 32, 32);
+
+          // game.debug.text(game.time.physicsElapsed, 32, 32);
+          // game.debug.body(player);
+          // game.debug.bodyInfo(player, 16, 24);
+
+
+
+
+
+
+
+      },
+
+      particleBurst: function(pointer) {
+
+          //  Position the emitter where the mouse/touch event was
+          emitter.x = pointer.x;
+          emitter.y = pointer.y;
+
+          //  The first parameter sets the effect to "explode" which means all particles are emitted at once
+          //  The second gives each particle a 2000ms lifespan
+          //  The third is ignored when using burst/explode mode
+          //  The final parameter (10) is how many particles will be emitted in this single burst
+          emitter.start(true, 2000, null, 10);
+      Meteor.call('click');
+
+      },
+
+  }
+
+
+  var map =  {
+
+      preload: function() {
+
+          game.load.image('dude', '/hero.png');
+          game.load.image('background', '/background.jpg');
+          game.load.image('shack', '/assets/shack.png');
+
+      },
+
+
+      create: function() {
+         game.world.setBounds(0, 0, 1920, 600);
+           game.physics.startSystem(Phaser.Physics.ARCADE);
+
+          game.time.desiredFps = 30;
+
+          bg = game.add.tileSprite(0, 0, 800, 600, 'background');
+
+          game.physics.arcade.gravity.y = 250;
+
+          player = game.add.sprite(20, 32, 'dude');
+          shack = game.add.sprite(30, 550, 'shack');
+          game.physics.enable(player, Phaser.Physics.ARCADE);
+
+          player.body.bounce.y = 0.2;
+          player.body.collideWorldBounds = true;
+          player.body.setSize(40, 125);
+
+
+
+          cursors = game.input.keyboard.createCursorKeys();
+          jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        game.camera.follow(player);
+      },
+
+      update:  function() {
+
+          // game.physics.arcade.collide(player, layer);
+
+          player.body.velocity.x = 0;
+
+          if (cursors.left.isDown)
+          {
+              player.body.velocity.x = -150;
+
+              if (facing != 'left')
+              {
+                  player.animations.play('left');
+                  facing = 'left';
+              }
+          }
+          else if (cursors.right.isDown)
+          {
+              player.body.velocity.x = 150;
+
+              if (facing != 'right')
+              {
+                  player.animations.play('right');
+                  facing = 'right';
+   game.state.start('Fields');
+              }
+          }
+
+          else
+          {
+              if (facing != 'idle')
+              {
+                  player.animations.stop();
+
+                  if (facing == 'left')
+                  {
+                      player.frame = 0;
+                  }
+                  else
+                  {
+                      player.frame = 5;
+                  }
+
+                  facing = 'idle';
+              }
+          }
+
+          if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer)
+          {
+              player.body.velocity.y = -250;
+              jumpTimer = game.time.now + 750;
+          }
+
+          else if (cursors.up.isDown)
+                {
+                      if (game.physics.arcade.overlap(player, shack)) {
+
+                       game.state.start('Fields');
+        }
+
+                }
+                else
+                {
+                    if (facing != 'idle')
+                    {
+                        player.animations.stop();
+
+                        if (facing == 'left')
+                        {
+                            player.frame = 0;
+                        }
+                        else
+                        {
+                            player.frame = 5;
+                        }
+
+                        facing = 'idle';
+                    }
+                }
+
+      },
+
+      render:  function() {
+
+          game.debug.text(game.time.suggestedFps, 32, 32);
+      },
+  }
+
+        var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'gamediv');
+      game.state.add('Map', map)
+  game.state.add('Fields', fields)
+       game.state.start('Map');
+
+     }
 
 
 Meteor.startup(function() {
@@ -296,6 +592,11 @@ Template.store.helpers({
   Template.reward.items = function() {
     return Meteor.user();
   }
+
+  Template.game.user = function() {
+    return Meteor.user();
+  }
+
   Template.profile.user = function() {
     return Meteor.user();
   }
@@ -402,298 +703,7 @@ Template.store.helpers({
 
   Template.game.rendered = function(){
     if (!this.rendered) {
-var emitter;
-    var player;
-     var facing = 'left';
-     var jumpTimer = 0;
-     var cursors;
-     var jumpButton;
-     var bg;
-var fields =  {
 
-    preload: function() {
-
-        game.load.image('dude', '/hero.png');
-        game.load.image('background', '/background.jpg');
-        game.load.image('shack', '/assets/shack.png');
-game.load.image('gold', 'assets/gold.png');
-
-    },
-
-    create: function() {
-   game.physics.startSystem(Phaser.Physics.ARCADE);
-
-
-game.stage.backgroundColor = 0x337799;
-
-
-emitter = game.add.emitter(0, 0, 100);
-
-
-emitter.makeParticles('gold');
-
-emitter.gravity = 200;
-
-
-game.input.onDown.add(particleBurst, this);
-       game.world.setBounds(0, 0, 1920, 600);
-         game.physics.startSystem(Phaser.Physics.ARCADE);
-
-        game.time.desiredFps = 30;
-
-        bg = game.add.tileSprite(0, 0, 800, 600, 'background');
-
-        game.physics.arcade.gravity.y = 250;
-
-        player = game.add.sprite(20, 32, 'dude');
- shack = game.add.sprite(20, 32, 'dude');
-
-        game.physics.enable(player, Phaser.Physics.ARCADE);
-
-        player.body.bounce.y = 0.2;
-        player.body.collideWorldBounds = true;
-        player.body.setSize(40, 125);
-
-
-
-        cursors = game.input.keyboard.createCursorKeys();
-        jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-      game.camera.follow(player);
-    },
-
-
-
-
-    update:  function() {
-
-        // game.physics.arcade.collide(player, layer);
-
-        player.body.velocity.x = 0;
-
-        if (cursors.left.isDown)
-        {
-            player.body.velocity.x = -150;
-
-            if (facing != 'left')
-            {
-                player.animations.play('left');
-                facing = 'left';
-            }
-        }
-        else if (cursors.right.isDown)
-        {
-            player.body.velocity.x = 150;
-
-            if (facing != 'right')
-            {
-                player.animations.play('right');
-                facing = 'right';
-            }
-        }
-	else if (cursors.up.isDown )
-        {
-
-
-                window.location.href = "https://questgrinders.herokuapp.com/store";
-
-        }
-        else
-        {
-            if (facing != 'idle')
-            {
-                player.animations.stop();
-
-                if (facing == 'left')
-                {
-                    player.frame = 0;
-                }
-                else
-                {
-                    player.frame = 5;
-                }
-
-                facing = 'idle';
-            }
-        }
-
-        if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer)
-        {
-            player.body.velocity.y = -250;
-            jumpTimer = game.time.now + 750;
-        }
-
-    },
-
-    render:  function() {
-
-        game.debug.text(game.time.suggestedFps, 32, 32);
-
-        // game.debug.text(game.time.physicsElapsed, 32, 32);
-        // game.debug.body(player);
-        // game.debug.bodyInfo(player, 16, 24);
-
-
-
-
-
-
-
-    },
-}
-
-function particleBurst(pointer) {
-
-    //  Position the emitter where the mouse/touch event was
-    emitter.x = pointer.x;
-    emitter.y = pointer.y;
-
-    //  The first parameter sets the effect to "explode" which means all particles are emitted at once
-    //  The second gives each particle a 2000ms lifespan
-    //  The third is ignored when using burst/explode mode
-    //  The final parameter (10) is how many particles will be emitted in this single burst
-    emitter.start(true, 2000, null, 10);
-Meteor.call('click');
-
-},
-var map =  {
-
-    preload: function() {
-
-        game.load.image('dude', '/hero.png');
-        game.load.image('background', '/background.jpg');
-        game.load.image('shack', '/assets/shack.png');
-
-    },
-
-
-    create: function() {
-       game.world.setBounds(0, 0, 1920, 600);
-         game.physics.startSystem(Phaser.Physics.ARCADE);
-
-        game.time.desiredFps = 30;
-
-        bg = game.add.tileSprite(0, 0, 800, 600, 'background');
-
-        game.physics.arcade.gravity.y = 250;
-
-        player = game.add.sprite(20, 32, 'dude');
-        shack = game.add.sprite(30, 550, 'shack');
-        game.physics.enable(player, Phaser.Physics.ARCADE);
-
-        player.body.bounce.y = 0.2;
-        player.body.collideWorldBounds = true;
-        player.body.setSize(40, 125);
-
-
-
-        cursors = game.input.keyboard.createCursorKeys();
-        jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-      game.camera.follow(player);
-    },
-
-    update:  function() {
-
-        // game.physics.arcade.collide(player, layer);
-
-        player.body.velocity.x = 0;
-
-        if (cursors.left.isDown)
-        {
-            player.body.velocity.x = -150;
-
-            if (facing != 'left')
-            {
-                player.animations.play('left');
-                facing = 'left';
-            }
-        }
-        else if (cursors.right.isDown)
-        {
-            player.body.velocity.x = 150;
-
-            if (facing != 'right')
-            {
-                player.animations.play('right');
-                facing = 'right';
- game.state.start('Fields');
-            }
-        }
-
-        else
-        {
-            if (facing != 'idle')
-            {
-                player.animations.stop();
-
-                if (facing == 'left')
-                {
-                    player.frame = 0;
-                }
-                else
-                {
-                    player.frame = 5;
-                }
-
-                facing = 'idle';
-            }
-        }
-
-        if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer)
-        {
-            player.body.velocity.y = -250;
-            jumpTimer = game.time.now + 750;
-        }
-
-        else if (cursors.up.isDown)
-              {
-                    if (game.physics.arcade.overlap(player, shack)) {
-
-                     game.state.start('Fields');
-      }
-
-              }
-              else
-              {
-                  if (facing != 'idle')
-                  {
-                      player.animations.stop();
-
-                      if (facing == 'left')
-                      {
-                          player.frame = 0;
-                      }
-                      else
-                      {
-                          player.frame = 5;
-                      }
-
-                      facing = 'idle';
-                  }
-              }
-
-    },
-
-    render:  function() {
-
-        game.debug.text(game.time.suggestedFps, 32, 32);
-
-        // game.debug.text(game.time.physicsElapsed, 32, 32);
-        // game.debug.body(player);
-        // game.debug.bodyInfo(player, 16, 24);
-
-
-
-
-
-
-
-    },
-}
-
-      var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'gamediv');
-    game.state.add('Map', map)
-game.state.add('Fields', fields)
-     game.state.start('Map');
 
       this.rendered = true;
     }
